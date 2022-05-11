0ba90548997d3e59473a653d259de4a1871b318f
AlexFlores-TrainingAssureSoft <alex.flores@training.assuresoft.com>
2022-05-11 13:49:10 -0400

    first commit spring

---
+++ src/main/java/com/example/alex/demoAlex/DemoAlexApplication.java
@@ -0,0 +1,13 @@
+package com.example.alex.demoAlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAlexApplication.class, args);
+	}
+
+}
